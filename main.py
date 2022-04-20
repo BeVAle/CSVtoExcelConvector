d5cad5e6e7b5ae674b11388c7846a1a169d0eb68
unknown <vlad.rotkopf@gmail.com>
2022-04-20 16:14:22 +0500

    Some mods

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 import sys
+
 from PyQt6 import QtWidgets
 
 import design
-import os
+
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
-    def __init__(self):
-
+    def __init__(self, filename=None):
         super().__init__()
         self.setupUi(self)
         self.SelectFileButton.clicked.connect(self.browse_folder)
+        self.ConvertorButton.clicked.connect(self.convert_file)
+        self.__filename = filename
+
+    @property
+    def filename(self):
+        return self.__filename
+
+    @filename.setter
+    def filename(self, new):
+        self.__filename = new
 
     def browse_folder(self):
-        directory = QtWidgets.QFileDialog.getOpenFileName(self, "Выберите файл")
+        file = QtWidgets.QFileDialog.getOpenFileName(self, "Выберите файл")
 
-        if directory[0]:
-            self.lineEdit.setText(directory[0])
+        if file[0]:
+            self.filename = file[0]
+            self.lineEdit.setText(file[0])
+
+    def convert_file(self):
+        self.lineEdit.setText("Алесики")
+        handle = open(self.filename, "r")
+        return
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = ExampleApp()  
+    window = ExampleApp()
     window.show()
     app.exec()
 
